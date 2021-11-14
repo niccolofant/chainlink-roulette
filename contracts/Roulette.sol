418d95610133e95cbff66e92e6aef28aa513d645
ocintnaf <ocintnaf@gmail.com>
2021-11-14 09:56:02 +0100

    Update Roulette.sol

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -30,13 +30,12 @@ contract Roulette is VRFConsumerBase, Ownable {
      * LINK token address:                0x01BE23585060835E02B77ef475b0Cc51aA1e0709
      * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
      */
-    constructor()
-        VRFConsumerBase(
-            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator address
-            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token address
-        )
-    {
-        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+    constructor(
+        address _coordinatorAddress,
+        address _linkTokenAddress,
+        bytes32 _keyHash
+    ) VRFConsumerBase(_coordinatorAddress, _linkTokenAddress) {
+        keyHash = _keyHash;
         fee = 0.1 * 10**18; // 0.1 LINK
     }
 
@@ -98,28 +97,23 @@ contract Roulette is VRFConsumerBase, Ownable {
         override
     {
         randomResult = randomness;
-
-        //load bet from memory
         Bet memory _curBet = bookOfBets[requestId];
         uint256 _betNum = _curBet.betNum;
         address payable _bettor = _curBet.bettor;
         uint256 _amount = _curBet.betAmount;
-
-        //calculate spin result
-        uint256 _spinResult = randomResult % 33;
+        uint256 _spinResult = randomResult % 37;
 
         //display spin result to public (only works if low volume)
         spinResult = _spinResult;
 
         //pay if they are a winner
         if (_spinResult == _betNum) {
-            (bool sent, ) = _bettor.call{value: _amount * 32}("");
+            (bool sent, ) = _bettor.call{value: _amount * 36}("");
             require(sent, "failed to send ether :(");
         }
         modifyMaxBet();
 
         //delete bet from memory
-
         delete bookOfBets[requestId];
     }
 }
